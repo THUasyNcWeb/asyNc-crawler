75566fff3954dda3289a0c655f5d733a39810710
kming <kming_l@163.com>
2022-10-04 20:27:48 +0800

    :sparkles: feat: create the NewsCrawlerItemLoader

--- news_crawler/news_crawler/items.py
+++ news_crawler/news_crawler/items.py
@@ -1,11 +1,19 @@
 import scrapy
+from scrapy.loader import ItemLoader
+from itemloaders.processors import MapCompose, TakeFirst, Identity
+
+
+class NewsCrawlerItemLoader(ItemLoader):
+    default_output_processor = TakeFirst()
 
 
 class NewsCrawlerItem(scrapy.Item):
     news_url = scrapy.Field()
     category = scrapy.Field()
     source = scrapy.Field()
-    key_words = scrapy.Field()
+    key_words = scrapy.Field(
+        output_processor=Identity()
+    )
     title = scrapy.Field()
     summary = scrapy.Field()
     content = scrapy.Field()
