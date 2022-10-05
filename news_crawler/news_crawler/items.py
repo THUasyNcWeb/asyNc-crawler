6fbe5e39ad54f72c9904c55b39055ea1a673f49f
kming <kming_l@163.com>
2022-10-05 12:57:26 +0800

    :bug: fix: adapt to the type of the data table

--- news_crawler/news_crawler/items.py
+++ news_crawler/news_crawler/items.py
@@ -9,13 +9,15 @@ class NewsCrawlerItemLoader(ItemLoader):
 
 class NewsCrawlerItem(scrapy.Item):
     news_url = scrapy.Field()
-    category = scrapy.Field()
-    source = scrapy.Field()
-    key_words = scrapy.Field(
+    category = scrapy.Field(
+        output_processor=Identity()
+    )
+    media = scrapy.Field()
+    tags = scrapy.Field(
         output_processor=Identity()
     )
     title = scrapy.Field()
-    summary = scrapy.Field()
+    description = scrapy.Field()
     content = scrapy.Field()
     first_img_url = scrapy.Field()
-    date = scrapy.Field()
+    pub_time = scrapy.Field()
