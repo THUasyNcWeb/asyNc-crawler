14b5adda979d40247f63670f5da4841d70aa2c56
kming <kming_l@163.com>
2022-10-05 13:43:48 +0800

    :sparkles: feat: add JsonExporterPipleline

--- news_crawler/news_crawler/pipelines.py
+++ news_crawler/news_crawler/pipelines.py
@@ -1,15 +1,31 @@
-from itemadapter import ItemAdapter
+from scrapy.exporters import JsonItemExporter
 
 
 class NewsCrawlerPipeline:
     def process_item(self, item, spider):
-        return item
+        pass
 
     def open_spider(self, spider):
-        self.file = open('test.html', 'w', encoding='utf-8')
+        pass
 
     def close_spider(self, spider):
+        pass
+
+    def process_item(self, item, spider):
+        pass
+
+
+class JsonExporterPipleline(object):
+    def __init__(self):
+        self.file = open('test.json', 'wb')
+        self.exporter = JsonItemExporter(
+            self.file, encoding="utf-8", ensure_ascii=False)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
         self.file.close()
 
     def process_item(self, item, spider):
-        self.file.write(str(item['text']))
+        self.exporter.export_item(item)
+        return item
