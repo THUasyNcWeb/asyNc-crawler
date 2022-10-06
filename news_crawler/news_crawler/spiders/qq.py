5f05382ce70f32a85d9c26e640a5113a1fc19c8f
kming <kming_l@163.com>
2022-10-06 11:51:05 +0800

    :bug: fix: fix item['content'] is NULL

--- news_crawler/news_crawler/spiders/qq.py
+++ news_crawler/news_crawler/spiders/qq.py
@@ -55,11 +55,13 @@ class QqSpider(scrapy.Spider):
         for tag in re.findall('"tags": "(.*?)"', window_data)[0].split(','):
             item_loader.add_value('tags', tag)
         item_loader.add_xpath('title', '/html/head/title/text()')
-        item_loader.add_xpath('description', '/html/head/meta[2]/text()')
+        item_loader.add_xpath('description', '/html/head/meta[2]/@content')
         item_loader.add_value('first_img_url', response.meta.get('image_url'))
-        item_loader.add_xpath('pub_time', '/html/head/meta[3]/@content')
-
-        for para in response.xpath('/html/body/div[3]/div[1]/div[1]/div[2]/p/text()').extract():
+        item_loader.add_value('pub_time', re.findall('"pubtime": "(.*?)"', window_data)[0])
+        paras = response.xpath('/html/body/div[3]/div[1]/div[1]/div[2]/p/text()').extract()
+        if len(paras) == 0:
+            paras.append('')
+        for para in paras:
             item_loader.add_value('content', para)
 
         yield item_loader.load_item()
