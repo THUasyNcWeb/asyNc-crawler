2aebce8a580ec25ceb4ec6b94458097e2dec0d2d
kming <kming_l@163.com>
2022-10-13 13:00:26 +0800

    :seedling: feat: choose whether update the test asyNc/asyNc-web#25

--- news_crawler/news_crawler/settings.py
+++ news_crawler/news_crawler/settings.py
@@ -49,8 +49,10 @@ COOKIES_ENABLED = False
 # Enable or disable spider middlewares
 # See https://docs.scrapy.org/en/latest/topics/spider-middleware.html
 # SPIDER_MIDDLEWARES = {
-#    'news_crawler.middlewares.NewsCrawlerSpiderMiddleware': 543,
+#    'scrapy_testmaster.TestMasterMiddleware': 950
 # }
+
+# TESTMASTER_ENABLED = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
