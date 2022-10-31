65a3e6bda57ee3334a1e9a64235116cf0f6fb6a3
kming <kming_l@163.com>
2022-11-01 00:49:26 +0800

    :sparkles: feat: implement dynamic url insertion for Xinhua asyNc-web#130

--- news_crawler/news_crawler/utils/utils.py
+++ news_crawler/news_crawler/utils/utils.py
@@ -54,6 +54,47 @@ class TencentIncrementTimer():
                                 json.dumps(start))
 
 
-if __name__ == '__main__':
-    tencent_timer = TencentIncrementTimer()
-    tencent_timer.execute()
+class XinhuaIncrementTimer():
+    """
+    Xinhua news crawler incremental crawl timer.
+    """
+
+    def __init__(self) -> None:
+        """
+        Init the timer to connect with redis.
+        """
+        with open('../config/redis.json', 'r', encoding='utf-8') as file:
+            config = json.load(file)
+        host = config['host']
+        port = config['port']
+        password = config['password']
+        self.my_redis = redis.Redis(host=host,
+                                    port=port,
+                                    decode_responses=True,
+                                    password=password)
+        with open('./web_news_config.json', 'r', encoding='utf-8') as file:
+            urls = json.load(file)
+        self.start_urls = urls['xinhua_news']
+
+    def execute(self):
+        """
+        Trigger timer.
+        """
+        self.add_job()
+        while True:
+            if len(self.my_redis.lrange('XinhuaNewsIncre:start_urls', 0, 1)) \
+                    == 0:
+                self.add_job()
+            time.sleep(1)
+
+    def add_job(self):
+        """
+        Add job into redis.
+        """
+        logging.info('Insert start_urls into XinhuaNewsIncre:start_urls')
+        for start_url in self.start_urls:
+            start = {}
+            start['url'] = start_url
+            start['meta'] = {'crawler': 'XinhuaNewsIncre'}
+            self.my_redis.lpush('XinhuaNewsIncre:start_urls',
+                                json.dumps(start))
